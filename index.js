2d5d6b849f5cd1ad8e8552f1f03030293d305f90
Robert Snyder <r.snyder49@gmail.com>
2018-12-23 02:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function logDriverNames(array) {
+  array.forEach(function(arr) {
+    console.log(arr.name);
+  });
+}
+  
+function logDriversByHometown(array, town) {
+  array.forEach(function(arr) {
+    if (arr.hometown === town) {
+      console.log(arr.name);
+    }
+  });
+}
+
+function driversByRevenue(array, )
