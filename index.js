51cbe1ba15cf63b46ec43c16813834ecbaacf010
Robert Snyder <r.snyder49@gmail.com>
2018-12-23 02:36:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logDriversByHometown(array, town) {
 }
 
 function driversByRevenue(array) {
-  let newDrivers = Object.create(drivers);
-  return newDrivers.sort(function(a, b) {
+  let newArr = Object.create(array);
+  return newArr.sort(function(a, b) {
     return a.revenue - b.revenue;
   });
 }
